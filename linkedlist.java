02014d24e0d06e61bde8f12499a1361e5b98075a
DharmavaramBhanuTejaReddy <bhanutejareddydharmavaram@gmail.com>
2025-08-19 14:28:24 +0530

    Create linkedlist.java

---
+++ linkedlist.java
@@ -0,0 +1,49 @@
+public class LinkedList {
+
+    Node head; // List head reference
+
+    // Node definition
+    static class Node {
+        int data;
+        Node next;
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    // Add new node at the end
+    public static LinkedList insert(LinkedList list, int data) {
+        Node new_node = new Node(data);
+        if (list.head == null) {
+            list.head = new_node;
+        } else {
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = new_node;
+        }
+        return list;
+    }
+
+    // Print all elements
+    public static void printList(LinkedList list) {
+        Node currNode = list.head;
+        System.out.print("LinkedList: ");
+        while (currNode != null) {
+            System.out.print(currNode.data + " ");
+            currNode = currNode.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+
+        printList(list); // Output: LinkedList: 1 2 3
+    }
+}
