57496dacedef64bab38f6ba2856cfabbbe2e8337
DharmavaramBhanuTejaReddy <bhanutejareddydharmavaram@gmail.com>
2025-08-19 14:35:47 +0530

    Update mergesort.java

--- mergesort.java
+++ mergesort.java
@@ -1,21 +1,13 @@
 public class MergeSort {
-    // Merges two subarrays of arr[]
     void merge(int arr[], int l, int m, int r) {
-        // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
-
-        // Create temp arrays
         int L[] = new int[n1];
         int R[] = new int[n2];
-
-        // Copy data to temp arrays
         for (int i = 0; i < n1; ++i)
             L[i] = arr[l + i];
         for (int j = 0; j < n2; ++j)
             R[j] = arr[m + 1 + j];
-
-        // Merge the temp arrays
         int i = 0, j = 0;
         int k = l;
         while (i < n1 && j < n2) {
@@ -28,22 +20,17 @@ public class MergeSort {
             }
             k++;
         }
-
-        // Copy remaining elements of L[]
         while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
-        // Copy remaining elements of R[]
         while (j < n2) {
             arr[k] = R[j];
             j++;
             k++;
         }
     }
-
-    // Main function to sort arr[l...r] using merge()
     void sort(int arr[], int l, int r) {
         if (l < r) {
             int m = (l + r) / 2;
@@ -52,8 +39,6 @@ public class MergeSort {
             merge(arr, l, m, r);
         }
     }
-
-    // Driver code to test the above
     public static void main(String args[]) {
         int arr[] = { 12, 11, 13, 5, 6, 7 };
         MergeSort ob = new MergeSort();
